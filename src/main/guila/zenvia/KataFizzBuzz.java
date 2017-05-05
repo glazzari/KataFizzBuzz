07b5592019da6a46a54d9610effebba671ded55b
Guilherme Lazzari <guilhermelazzari@gmail.com>
2017-05-04 21:42:59 -0300

    Add improved version to support different FizzBuzz games.

--- src/main/guila/zenvia/KataFizzBuzz.java
+++ src/main/guila/zenvia/KataFizzBuzz.java
@@ -3,47 +3,10 @@ package main.guila.zenvia;
 /**
  * This class implements the FizzBuzz game.
  */
-public class KataFizzBuzz {
-
-  private final String labelDiv3;
-  private final String labelDiv5;
-
-  public KataFizzBuzz(String labelDiv3, String labelDiv5) {
-    this.labelDiv3 = labelDiv3;
-    this.labelDiv5 = labelDiv5;
-  }
-
+public class KataFizzBuzz extends KataGame {
   public KataFizzBuzz() {
-    this("Fizz", "Buzz");
-  }
-
-  private boolean isDivisibleBy3(int n) {
-    return n % 3 == 0;
-  }
-
-  private boolean isDivisibleBy5(int n) {
-    return n % 5 == 0;
-  }
-
-  /**
-   * Evaluate a number as follows:
-   * - for multiples of three print "Fizz"
-   * - for multiples of five print "Buzz"
-   * - for numbers which are multiples of both three and five print "FizzBuzz"
-   * - otherwise, print the number
-   * @param number
-   * @return String
-   */
-  public String evaluate(int number) {
-    if (isDivisibleBy3(number) && isDivisibleBy5(number)) {
-      return labelDiv3 + labelDiv5;
-    } else if (isDivisibleBy3(number)) {
-      return labelDiv3;
-    } else if (isDivisibleBy5(number)) {
-      return labelDiv5;
-    } else {
-      return String.valueOf(number);
-    }
+    addMatcher("Fizz", 3);
+    addMatcher("Buzz", 5);
   }
 }
 
--- src/main/guila/zenvia/KataGame.java
+++ src/main/guila/zenvia/KataGame.java
@@ -1,4 +1,4 @@
-package main.guila.zenvia.advanced;
+package main.guila.zenvia;
 
 import java.util.ArrayList;
 import java.util.List;
